1b0f1bd9f53c156c966c8d7988b96da9dbd8e1a8
Xotic750 <xotic750@gmail.com>
2017-08-11 22:03:25 +0100

    Upgrade isObjectLike

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * @file Truncate a string to a maximum specified length.
- * @version 1.3.0
+ * @version 1.4.0
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
